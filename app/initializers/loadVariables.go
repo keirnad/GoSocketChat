928eeb1ec13dbdbd8b98625b0ef5ab984933e5d1
Dzedz Timofei <dzedz@internet.ru>
2026-07-26 22:05:41 +0300

    Changed variables load

--- app/initializers/loadVariables.go
+++ app/initializers/loadVariables.go
@@ -7,8 +7,8 @@ import (
 )
 
 func LoadVariables() {
-	err := godotenv.Load()
+	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err)
 	}
 }
